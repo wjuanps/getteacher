5837ecbad130e815f9e0d32fc7ace269f7a07be0
Juan Soares <wjuan.ps@gmail.com>
2020-05-01 22:02:29 -0300

    Update home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import Header from "./pages/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <Header />
+    <Home />
   );
 }
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from "../Header";
+
+const Home = () => {
+  return (
+    <div className="homepage">
+      <div id="page-wrapper">
+        <Header />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
