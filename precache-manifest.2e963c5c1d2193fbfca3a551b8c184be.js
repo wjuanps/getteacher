63c2d6d20a8982df66a78e62c063eb657b6586c7
Juan Soares <wjuan.ps@gmail.com>
2020-05-03 10:33:48 -0300

    Updates

---
+++ precache-manifest.2e963c5c1d2193fbfca3a551b8c184be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584c54b0723301d035ad1c447bd21978",
+    "url": "/getteacher/index.html"
+  },
+  {
+    "revision": "c7782a6399f85ea32148",
+    "url": "/getteacher/static/css/main.774c1c1d.chunk.css"
+  },
+  {
+    "revision": "ed390f990ce3acb076ec",
+    "url": "/getteacher/static/js/2.8cd84c9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/getteacher/static/js/2.8cd84c9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7782a6399f85ea32148",
+    "url": "/getteacher/static/js/main.35914ce2.chunk.js"
+  },
+  {
+    "revision": "17ef025fe91905636386",
+    "url": "/getteacher/static/js/runtime-main.9b153b22.js"
+  }
+]);
